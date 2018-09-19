eb3ee5f3bada5040a2c2de921a19f0edaec81c2e
Octavi Roset <octaviii@gmailll.com>
2018-09-19 18:50:03 +0200

    Text en angles.

--- suma.java
+++ suma.java
@@ -2,8 +2,8 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introduce el primer numero:");
+System.out.println("Input the first number:");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce el segundo numero:");
+System.out.println("Input the second number:");
 numero2 = reader.nextInt();
