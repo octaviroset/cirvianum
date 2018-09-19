cfdb9c0dfb0bbf5c9ed37eac0541a3d8427085fc
Octavi Roset <octaviii@gmailll.com>
2018-09-19 18:53:26 +0200

    Text en catala

--- suma.java
+++ suma.java
@@ -2,8 +2,8 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introduce el primer numero:");
+System.out.println("Introdueix el primer numero:");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce el segundo numero:");
+System.out.println("Introdueix el segon numero:");
 numero2 = reader.nextInt();
